942e1ff2c009aa36e95fde1b9920606e582632f7
belugaman <littledage@outlook.com>
2025-09-25 20:55:55 -0700

    Update AxisOrient.py

--- AxisOrient.py
+++ AxisOrient.py
@@ -1,1 +1,13 @@
 
+from vpython import *
+import numpy as np
+
+arrowL = 2
+arrowT = 0.02
+
+Xarrow = arrow(axis = vector(1, 0, 0), color = color.red, lenght = arrowL, shaftwidth = arrowT)
+Yarrow = arrow(axis = vector(0, 1, 0), color = color.red, lenght = arrowL, shaftwidth = arrowT)
+Zarrow = arrow(axis = vector(0, 0, 1), color = color.red, lenght = arrowL, shaftwidth = arrowT)
+
+while True:
+  pass
